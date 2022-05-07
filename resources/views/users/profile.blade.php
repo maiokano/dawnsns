8d345150fb24bbbb98f9ed3d5909ea8afae3e75d
岡野真依 <>
2022-05-07 22:15:55 +0900

    相手のプロフィール作成中

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -2,6 +2,26 @@
 
 @section('content')
 
+<div class='profile_wrapper'>
+    @foreach($users as $user)
+   <img src="/images/{{$user->images}}">
+   <p>{{$user->username}}</p>
+   <p>{{$user->bio}}</p>
+    @if(!in_array($user->id,array_column($follow_list,'follow')))
+      <a><button type="submit" onclick="location.href=`/follow/{{$user->id}}`" class="btn btn-success pull-right">フォローする</button></a>
+      @else
+      <a><button type="submit" onclick="location.href=`/unfollow/{{$user->id}}`" class="btn btn-success pull-right">フォローをはずす</button></a>
+    @endif
+    @endforeach
+</div>
+<div class='posts_wrapper'>
+    @foreach($users as $user)
+            <img src="images/{{$user->images}}">
+            <p>{{$user->username}}</p>
+            <p>{{$_post->posts}}</p>
+            <p>{{$->created_at}}</p>
+    @endforeach
+</div>
 
 
 @endsection
